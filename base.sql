adf90fbb44f8fcb2755797feb1d2563b01963b98
空影星辉 <wlgc1801lzh@163.com>
2021-08-29 11:01:44 +0800

    更新数据库文件

--- base.sql
+++ base.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 5.7.29, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: base
 -- ------------------------------------------------------
--- Server version	5.7.29
+-- Server version	8.0.26
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,15 +21,16 @@
 
 DROP TABLE IF EXISTS `menu`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `menu` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键自增ID',
-  `pid` int(11) DEFAULT NULL COMMENT '父级菜单ID',
-  `name` varchar(30) DEFAULT NULL COMMENT '菜单名称',
-  `path` varchar(30) DEFAULT NULL COMMENT '菜单路径',
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键自增ID',
+  `pid` int DEFAULT NULL COMMENT '父级菜单ID',
+  `title` varchar(30) DEFAULT NULL COMMENT '菜单名称',
+  `href` varchar(30) DEFAULT NULL COMMENT '菜单路径',
   `icon` varchar(30) DEFAULT NULL COMMENT '菜单图标',
+  `target` varchar(30) DEFAULT NULL COMMENT '打开方式',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COMMENT='系统菜单表';
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='系统菜单表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +39,7 @@ CREATE TABLE `menu` (
 
 LOCK TABLES `menu` WRITE;
 /*!40000 ALTER TABLE `menu` DISABLE KEYS */;
-INSERT INTO `menu` (`id`, `pid`, `name`, `path`, `icon`) VALUES (1,0,'系统首页','dashboard','layui-icon-home'),(2,0,'系统设置','','layui-icon-set'),(3,2,'菜单管理','menu','layui-icon-spread-left'),(4,2,'角色管理','role','layui-icon-user'),(5,2,'用户管理','user','layui-icon-username'),(6,3,'查询菜单','menu/list','layui-icon-spread-left'),(7,3,'添加/修改菜单','menu/save','layui-icon-spread-left'),(8,3,'删除菜单','menu/delete','layui-icon-spread-left'),(9,4,'查询角色','role/list','layui-icon-group'),(10,4,'添加/修改角色','role/save','layui-icon-group'),(11,4,'删除角色','role/delete','layui-icon-group'),(12,5,'查询用户','user/list','layui-icon-username'),(13,5,'添加/修改用户','user/save','layui-icon-username'),(14,5,'删除用户','user/delete','layui-icon-username');
+INSERT INTO `menu` (`id`, `pid`, `title`, `href`, `icon`, `target`) VALUES (1,0,'系统首页','dashboard','fa fa-tachometer','_self'),(2,0,'系统管理','','fa fa-tachometer','_self'),(3,2,'菜单管理','menu','fa fa-tachometer','_self'),(4,2,'角色管理','role','fa fa-tachometer','_self'),(5,2,'用户管理','user','fa fa-tachometer','_self'),(6,2,'系统设置','setting','fa fa-tachometer','_self');
 /*!40000 ALTER TABLE `menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -48,13 +49,13 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `role`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `role` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键自增ID',
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键自增ID',
   `name` varchar(30) DEFAULT NULL COMMENT '角色名称',
   `remark` varchar(100) DEFAULT NULL COMMENT '角色备注',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,7 +64,7 @@ CREATE TABLE `role` (
 
 LOCK TABLES `role` WRITE;
 /*!40000 ALTER TABLE `role` DISABLE KEYS */;
-INSERT INTO `role` (`id`, `name`, `remark`) VALUES (1,'超级管理员','我是神'),(5,'普通用户','普通用户');
+INSERT INTO `role` (`id`, `name`, `remark`) VALUES (1,'超级管理员','我是神');
 /*!40000 ALTER TABLE `role` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -73,13 +74,13 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `role_menu`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `role_menu` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键自增ID',
-  `role_id` int(11) DEFAULT NULL COMMENT '角色ID',
-  `menu_id` int(11) DEFAULT NULL COMMENT '菜单ID',
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键自增ID',
+  `role_id` int DEFAULT NULL COMMENT '角色ID',
+  `menu_id` int DEFAULT NULL COMMENT '菜单ID',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COMMENT='角色权限关联表';
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='角色权限关联表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,7 +89,7 @@ CREATE TABLE `role_menu` (
 
 LOCK TABLES `role_menu` WRITE;
 /*!40000 ALTER TABLE `role_menu` DISABLE KEYS */;
-INSERT INTO `role_menu` (`id`, `role_id`, `menu_id`) VALUES (1,1,1),(2,1,2),(3,1,3),(4,1,4),(5,1,5),(10,5,1),(11,1,6),(12,1,7),(13,1,8),(14,1,9),(15,1,10),(16,1,11),(17,1,12),(18,1,13),(19,1,14);
+INSERT INTO `role_menu` (`id`, `role_id`, `menu_id`) VALUES (1,1,1),(2,1,2),(3,1,3),(4,1,4),(5,1,5);
 /*!40000 ALTER TABLE `role_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -98,9 +99,9 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID，自增',
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键ID，自增',
   `username` varchar(30) NOT NULL COMMENT '用户名',
   `password` varchar(30) NOT NULL COMMENT '密码',
   `gender` char(1) DEFAULT NULL COMMENT '性别',
@@ -108,7 +109,7 @@ CREATE TABLE `user` (
   `email` varchar(30) DEFAULT NULL COMMENT '电子邮箱',
   `avatar` varchar(100) DEFAULT NULL COMMENT '用户头像',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT='系统用户表';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='系统用户表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -117,7 +118,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`id`, `username`, `password`, `gender`, `phone`, `email`, `avatar`) VALUES (1,'admin','123456','男','15176125191','wlgc1801lzh@163.com','default_avatar.jpg'),(2,'test','123456','男','123','123','default_avatar.jpg');
+INSERT INTO `user` (`id`, `username`, `password`, `gender`, `phone`, `email`, `avatar`) VALUES (1,'admin','123456','男','15176125191','wlgc1801lzh@163.com','default_avatar.jpg');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -127,13 +128,13 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `user_role`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_role` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键自增ID',
-  `user_id` int(11) DEFAULT NULL COMMENT '用户ID',
-  `role_id` int(11) DEFAULT NULL COMMENT '角色ID',
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键自增ID',
+  `user_id` int DEFAULT NULL COMMENT '用户ID',
+  `role_id` int DEFAULT NULL COMMENT '角色ID',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT='用户角色关联表';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户角色关联表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -142,7 +143,7 @@ CREATE TABLE `user_role` (
 
 LOCK TABLES `user_role` WRITE;
 /*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
-INSERT INTO `user_role` (`id`, `user_id`, `role_id`) VALUES (1,1,1),(2,2,5);
+INSERT INTO `user_role` (`id`, `user_id`, `role_id`) VALUES (1,1,1);
 /*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -155,4 +156,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-08-05 11:18:06
+-- Dump completed on 2021-08-29 11:01:17
